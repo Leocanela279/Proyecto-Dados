7bb4a019eaf7877a7567e3a7ed9c99b8d440bbc2
Leandro Canela <lagarcia@alumnat.copernic.cat>
2025-02-20 16:28:18 +0100

    Primer proyecto con JavaScript

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+let randomNum1 = Math.floor(Math.random() * 6) + 1;
+let randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", `images/dau${randomNum1}.png`);
+document.querySelector(".img2").setAttribute("src", `images/dau${randomNum2}.png`);
+
+if (randomNum1 > randomNum2)
+    document.querySelector("h1").textContent = "Player 1 Wins! 🚩";
+else if (randomNum1 < randomNum2)
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+else
+    document.querySelector("h1").textContent = "Draw!";
